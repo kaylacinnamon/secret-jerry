747d27c84b9bf8534b1de11abc70efddda87db08
Jeremy Torella <jtorella@wayfair.com>
2019-12-23 00:40:04 -0500

    Add tests for players joining

--- src/app.js
+++ src/app.js
@@ -6,18 +6,45 @@ import openSocket from 'socket.io-client';
 
 class App extends Component {
 
+    state = {
+        roomID: null,
+        playerName: null
+    };
+
+
     handleCreateGame = () => {
         console.log('Opening Socket to server');
         const socket = openSocket('http://localhost:4001');
-
+        socket.emit('gameMaster')
     };
 
-    
+    handleRoomIDChange = (event) => {
+        this.setState({roomID: event.target.value});
+    }
+    handlePlayerNameChange = () => {
+        this.setState({playerName: event.target.value});
+    }
 
+    handleJoinGame = () => {
+        console.log('Joining Game...');
+        const socket = openSocket('http://localhost:4001');
+        socket.emit('playerJoin', this.state.playerName);
+    }
+   
     render() {
         return (
         <div className="App">
             <button onClick={this.handleCreateGame}>Create Game</button>
+            <br/>
+            <br/>
+            <div> Room ID </div>
+            <input name="Room ID" onChange={this.handleRoomIDChange}/>
+            <br/>
+            <br/>
+            <div> Name </div>
+            <input onChange={this.handlePlayerNameChange}/>
+            <br/>
+            <button onClick={this.handleJoinGame}>Join Game</button>
         </div>
         );
     }
--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
 const io = require("socket.io")();
 
 const port = process.env.PORT || 4001;
+const gameMasterSocketID = null;
+const players = {};
 
 io.on("connection", socket => {
-    console.log("New client connected");
+    console.log(`New client connected with client id ${socket.id}`);
     socket.on("disconnect", () => console.log("Client disconnected"));
+
+    socket.on("gameMaster", () => {gameMasterSocketID = socket.id});
+    socket.on("playerJoin", playerName => {
+        players[socket.id] = playerName;
+        console.dir(players);
+    });
 });
 
 io.listen(port);
