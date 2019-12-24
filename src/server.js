29d840cc88210c21f6596617451a0fa1bca2d9b4
Jeremy Torella <jtorella@wayfair.com>
2019-12-23 19:43:26 -0500

    Add boilerplate gameStart method

--- src/server.js
+++ src/server.js
@@ -2,7 +2,19 @@ const io = require("socket.io")();
 
 const port = process.env.PORT || 4001;
 const gameMasterSocketID = null;
-const players = {};
+const players = {}; // players[socketID] = playerName
+
+/**
+ * Called when the client presses 'Start Game'
+ */
+function handleGameStart() {}
+
+/**
+ * Assigns roles to all players
+ */
+function assignRoles() {
+    playerCount = len(players.keys());  
+}
 
 io.on("connection", socket => {
     console.log(`New client connected with client id ${socket.id}`);
@@ -13,6 +25,7 @@ io.on("connection", socket => {
         players[socket.id] = playerName;
         console.dir(players);
     });
+    socket.on("gameStart", handleGameStart);
 });
 
 io.listen(port);
