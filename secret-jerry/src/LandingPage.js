2b32a7265dc3a7a58ac59fd7029325b090f3502f
kaylacinnamon <kayla.m.cinnamon@gmail.com>
2019-12-26 23:46:44 -0500

    landing page styling

--- secret-jerry/src/LandingPage.js
+++ secret-jerry/src/LandingPage.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Button from 'react-bootstrap/Button';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
 
@@ -19,20 +18,43 @@ class LandingPage extends Component {
         return (
             <div id="landingPage">
                 <br/>
-                <Button onClick={this.props.handleCreateGame}>Create Game</Button>
-                <InputGroup size="sm" className="mb-3" onChange={this.props.handleRoomIDChange} value={this.props.roomID}>
-                    <InputGroup.Prepend>
-                        <InputGroup.Text id="inputGroup-sizing-sm" name="Room ID">Room ID</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl aria-label="Small" aria-describedby="inputGroup-sizing-sm" />
-                </InputGroup>
-                <InputGroup size="sm" className="mb-3" onChange={this.props.handlePlayerNameChange} value={this.props.playerName}>
-                    <InputGroup.Prepend>
-                        <InputGroup.Text id="inputGroup-sizing-sm">Name</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl aria-label="Small" aria-describedby="inputGroup-sizing-sm" />
-                </InputGroup>
-                <Button onClick={this.props.handleJoinGame}>Join Game</Button>
+                <div class="row">
+                    <div class="col">
+                        <div class="card bg-secondary mb-2">
+                            <div class="card-header">Create Game</div>
+                            <div class="card-body">
+                                <h4 class="card-title">Create a New Game</h4>
+                                <p class="card-text">Here's some info on how you can create a new game.</p>
+                                <button type="button" onClick={this.props.handleCreateGame} id="createGameButton" class="btn btn-primary">Create Game</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card text-white bg-primary mb-2">
+                            <div class="card-header">Join Game</div>
+                            <div class="card-body">
+                                <h4 class="card-title">Join a Game</h4>
+                                <p class="card-text">Here's what you need to know to join a game.</p>
+                                <InputGroup size="sm" className="mb-3" onChange={this.props.handleRoomIDChange} value={this.props.roomID}>
+                                    <InputGroup.Prepend>
+                                        <InputGroup.Text id="inputGroup-sizing-sm" name="Room ID">Room ID</InputGroup.Text>
+                                    </InputGroup.Prepend>
+                                    <FormControl aria-label="Small" aria-describedby="inputGroup-sizing-sm" />
+                                </InputGroup>
+                                <InputGroup size="sm" className="mb-3" onChange={this.props.handlePlayerNameChange} value={this.props.playerName}>
+                                    <InputGroup.Prepend>
+                                        <InputGroup.Text id="inputGroup-sizing-sm">Name</InputGroup.Text>
+                                    </InputGroup.Prepend>
+                                    <FormControl aria-label="Small" aria-describedby="inputGroup-sizing-sm" />
+                                </InputGroup>
+                                <button type="button" onClick={this.props.handleJoinGame} id="joinGameButton" class="btn btn-secondary">Join Game</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <br /><br />
+                
             </div>
         )
     }
